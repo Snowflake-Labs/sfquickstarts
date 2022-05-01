da5f6dea7dfb5c602ed88f0d7ff8120345c4334b
mcnayak <cmohan.nayak@gmail.com>
2022-05-01 10:56:48 -0700

    SQL script for Download

--- site/sfguides/src/visual_analytics_powered_by_snowflake_and_tableau/assets/MakeFinal_Workshop.sql
+++ site/sfguides/src/visual_analytics_powered_by_snowflake_and_tableau/assets/MakeFinal_Workshop.sql
@@ -1,81 +1,18 @@
-author: Chandra Nayak & Bailey Ferrari 
-id: visual_analytics_powered_by_snowflake_and_tableau
-summary: Visual Analytics Powered by Snowflake and Tableau
-categories: Getting Started
-environments: web
-status: Published 
-feedback link: https://github.com/Snowflake-Labs/sfguides/issues
-tags: Getting Started, Data Science, Data Engineering, Twitter 
+/*-------------------------------------------------------------------------------------------------------------------
+-- <VHOL SQL>
+-- <Embedded Analytics Applications Powered by Snowflake and Tableau>
+-- <October 14, 2020 | 11:00am PST>
+-- <SQL File |Chandra Nayak>
+-- <Sales Engineer | Snowflake>
+--  PDF: https://snowflake-corp-se-workshop.s3-us-west-1.amazonaws.com/VHOL_Embedded/Tableau_VHOL_IOT_Embedded.pdf
+--  SQL: https://snowflake-corp-se-workshop.s3-us-west-1.amazonaws.com/VHOL_Embedded/Sanitized_Workshop_SQL.sql
+-------------------------------------------------------------------------------------------------------------------*/
 
-# Visual Analytics powered by Snowflake and Tableau
-<!-- ------------------------ -->
-## Overview 
-Duration: 90
+/*--------------------------------------------------------Set Up---------------------------------------------------------*/
+--Role
+USE ROLE ACCOUNTADMIN;
 
-Join Snowflake and Tableau for an instructor-led hands-on lab to build governed, visual, and interactive analytics quickly and easily. 
-
-
-The rest of this Snowflake Guide explains the steps of writing your own guide. 
-
-### Prerequisites
-- Familiarity with Snowflake and Tableau
-
-### What You’ll Learn 
-- Load unstructured data from IoT enabled bikes into Snowflake.
-- Integrate and deliver multi-tenant tables and views in Snowflake to Tableau for real-time dashboarding.
-- Use API integration to load geospatial data into Snowflake. 
-- Securely connect to your Snowflake data by leveraging Virtual Connections and Centralized Row Level Security in Tableau.
-- Build visual, intuitive, and interactive data visualizations powered by live data in Snowflake.
-- Share production-ready Tableau dashboards by embedding the visualizations into your custom application.
-- Showcase your data in the Snowflake Data Marketplace
-
-
-### What You’ll Need 
-- A [Snowflake](https://trial.snowflake.com/) Account 
-- A [Tabelau Online](https://www.tableau.com/products/online/request-trial) Account
-
-
-### What You’ll Build 
-- Tableau Dashboard to present demand for cycles correlated with Weather
-
-<!-- ------------------------ -->
-## Snowflake Configuration
-Duration: 5
-1. Create a Snowflake Multi-cluster Trial account 
-
-2. Login to your Snowflake Trial account
-
-3. We will be using the new UI to get started but you can also switch over to the Classic Console if you would like. 
-
-Classic UI: 
-If you ever want to change from the new UI to the classic one, click on the home button and then Classic Console.
-
-
-### Images
-![Snowflake Login](assets/new_snowflake_ui.png)
-
-<!-- ------------------------ -->
-## Tableau Configuration
-Duration: 2
-1. Login to your Tableau Online Trial account
- 
-
-<!-- ------------------------ -->
-
-## Managing Unstructured Data
-Duration: 2
-
-Look at the [markdown source for this sfguide](https://raw.githubusercontent.com/Snowflake-Labs/sfguides/master/site/sfguides/sample.md) to see how to use markdown to generate code snippets, info boxes, and download buttons. 
-
-### Download Workload.sql 
-<button>
-  
-</button>
-
-### Create Snowflake Objects
-
-``` sql 
--- Create Database, Schema and Warehouse
+--Create Database, Schema, Warehouse, Stage and File Format
 create or replace database VHOL_DATABASE;
 use database VHOL_DATABASE;
 
@@ -91,16 +28,12 @@ create or replace warehouse VHOL_WH WITH
     MIN_CLUSTER_COUNT = 1 
     MAX_CLUSTER_COUNT = 1 
     SCALING_POLICY = 'STANDARD';
-  
--- Change Compute Size Instantly 
+    
 alter warehouse VHOL_WH SET WAREHOUSE_SIZE = 'LARGE';
 
 alter warehouse VHOL_WH SET WAREHOUSE_SIZE = 'MEDIUM';
 
-```
-
-### Create Stage for hosting files
-```sql
+--Internal Stage
 create or replace STAGE VHOL_STAGE;
 
 show stages;
@@ -115,32 +48,30 @@ list @VHOL_STAGE/;
 CREATE FILE FORMAT "JSON" TYPE=JSON COMPRESSION=GZIP;
 
 show File Formats;
-```
 
-### Query JSON Data
-``` sql
--- Query individual columns from first 100 rows 
+
+/*--------------------------------------------------------Trips Data---------------------------------------------------------*/
+--select over stage
 select $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16
 from @VHOL_STAGE/2016-08-01/data_01a304b5-0601-4bbe-0045-e8030021523e_005_6_0.json.gz limit 100;
 
--- Query all columns from a single row
+
 SELECT * FROM @VHOL_STAGE/2016-08-01/data_01a304b5-0601-4bbe-0045-e8030021523e_005_6_0.json.gz (file_format=>JSON)  limit 1;
 
--- Create a table with Variant column
+-- Lets create a table to bring this data into Snowflake from Object Store
 create or replace table vhol_trips
   (tripid number autoincrement, 
    v variant)
   change_tracking = true;
 
--- Load JSON data into Variant column
+--copy data over for trips
+--copy into trips from @VHOL_STAGE file_format=JSON;
+
 copy into vhol_trips (v) from 
   (SELECT * FROM @VHOL_STAGE/2016-08-01/data_01a304b5-0601-4bbe-0045-e8030021523e_005_6_0.json.gz (file_format=>JSON));
-```
 
-### Build Relational Views on JSON
-``` sql 
 
---- Extract JSON data as relational columns
+--create a view with rows and columns 
 create or replace view vhol_trips_vw 
   as select 
     tripid,
@@ -163,12 +94,16 @@ create or replace view vhol_trips_vw
       v:RIDER.PAYMENT.CC_NUM::string) payment_num
   from vhol_trips;
 
+
+
+--select data from 
 select * from vhol_trips_vw limit 20;
 
--- avg trip duration 
+--- fancy query -----
 select date_trunc('hour', starttime) as "date",
 count(*) as "num trips",
 avg(duration)/60 as "avg duration (mins)" 
+--avg(haversine(start_station_latitude, start_station_longitude, end_station_latitude, end_station_longitude)) as "avg distance (km)" 
 from vhol_trips_vw
 group by 1 order by 1;
 
@@ -179,20 +114,11 @@ select
 from vhol_trips_vw
 group by 1 order by 2 desc;
 
-``` 
-
-### 
-<!-- ------------------------ -->
-## DevOps in Snowflake
-
-### Clone Table
-``` sql 
+--clone
 create table vhol_trips_dev clone vhol_trips;
 
 select * from vhol_trips_dev limit 1;
-```
-### Drop and Undrop Table
-``` sql
+
 drop table vhol_trips_dev; 
 
 select count(*) from vhol_trips_dev limit 1;
@@ -202,36 +128,28 @@ undrop table vhol_trips_dev;
 
 select count(*) from vhol_trips_dev limit 1;
 
-```
-<!-- ------------------------ -->
-<!-- ------------------------ -->
-## Bring Weather Data from Snowflake Marketplace
+/*--------------------------------------------------------Get Marketplace Weather Data---------------------------------------------------------*/
 
-Weather Dataset: 
-Set database name to Weather, grant access to PUBLIC role
 
-### Convert Kelvin to Celcius
-``` sql
+desc view weather.standard_tile.history_day;
+
+select * from weather.standard_tile.history_day limit 1;
+
+-- Is there rain in the forecast that impact cycling
+SELECT COUNTRY,DATE_VALID_STD,TOT_PRECIPITATION_IN,tot_snowfall_in AS SNOWFALL,  POSTAL_CODE, DATEDIFF(day,current_date(),DATE_VALID_STD) AS DAY, HOUR(TIME_INIT_UTC) AS HOUR  FROM WEATHER.STANDARD_TILE.FORECAST_DAY WHERE POSTAL_CODE='32333' AND DAY=7;
+--select
+
 -- UDF to convert Kelvin to Celcius
 create or replace function degFtoC(k float)
 returns float
 as
 $$
   truncate((k - 32) * 5/9, 2)
-$$; 
+$$;
 
-```
-###
 
-### Query Weather data
-``` sql
-desc view weather.standard_tile.history_day;
 
--- Is there rain in the forecast that impact cycling in a specific area 
-SELECT COUNTRY,DATE_VALID_STD,TOT_PRECIPITATION_IN,tot_snowfall_in AS SNOWFALL,  POSTAL_CODE, DATEDIFF(day,current_date(),DATE_VALID_STD) AS DAY, HOUR(TIME_INIT_UTC) AS HOUR  FROM WEATHER.STANDARD_TILE.FORECAST_DAY WHERE POSTAL_CODE='32333' AND DAY=7;
-```
-### Is there precipitation or snowfall in NY zipcodes
-``` sql
+--create view
 create or replace view vhol_weather_vw as
   select 'New York'                                   state,
     date_valid_std                                    observation_date,
@@ -255,43 +173,33 @@ create or replace view vhol_weather_vw as
   where postal_code in ('10257', '10060', '10128', '07307', '10456')
   group by 1, 2, 3;
   
+  
+
+
+
+--select *
 select * from vhol_weather_vw limit 10;
 
-```
-<!-- ------------------------ -->
 
-<!-- ------------------------ -->
-## Get Geospatial Station Data 
-Let's get geospatial data for stations where cycles are available 
 
-###  Access data from AWS API Gateway
-``` sql  
--- Integration to AWS API Gateway 
+
+
+----- Now let's create station data by Chandra -----------
+-- create the API integration
 create or replace api integration fetch_http_data
   api_provider = aws_api_gateway
   api_aws_role_arn = 'arn:aws:iam::148887191972:role/ExecuteLambdaFunction'
   enabled = true
   api_allowed_prefixes = ('https://dr14z5kz5d.execute-api.us-east-1.amazonaws.com/prod/fetchhttpdata');
 
-```
 
-``` sql 
--- External Functions call to Lambda to download data 
-
+-- create an external function to call a Lambda that downloads data from a URL
 create or replace external function fetch_http_data(v varchar)
     returns variant
     api_integration = fetch_http_data
     as 'https://dr14z5kz5d.execute-api.us-east-1.amazonaws.com/prod/fetchhttpdata';
 
-```
-###
 
-### Flatten JSON data from API's
-
-Geospatial data is available in a nested json array, let's flatten that 
-
-``` sql 
--- use lateral flatten function to flatten nested JSON
 create or replace table vhol_spatial_data as
 with gbfs as (
   select $1 type, 
@@ -315,18 +223,8 @@ with gbfs as (
     
     
     select * from vhol_spatial_data;
-  ```
-
-###
-
-<!-- ------------------------ -->
-
-<!-- ------------------------ -->
-## Join Trips, Weather and Geospatial Data
-
-### Combine station data with geospatial data
-``` sql 
-create or replace table vhol_stations as with 
+    
+    create or replace table vhol_stations as with 
   -- extract the station data
     s as (select 
         v:station_id::number station_id,
@@ -359,15 +257,10 @@ select station_id, station_name, station_lat, station_lon, station_geo,
   nhood_name, borough_name, nhood_geo
 from s inner join r on s.region_id = r.region_id
        left outer join n on st_contains(n.nhood_geo, s.station_geo);
-
+       
 
 select * from vhol_stations;
-``` 
 
-### Combine Trips, Geospatial and Stations 
-
-Let's combine trip data with geospatial to identify popular routes
-```sql
 create or replace view vhol_trips_stations_vw as (
   with
     t as (select * from vhol_trips_vw),
@@ -389,13 +282,8 @@ create or replace view vhol_trips_stations_vw as (
   from t 
     left outer join ss on start_station_id = ss.station_id
     left outer join es on end_station_id = es.station_id); 
-
-```
-
-###  
-
-### Combine Trip, Geospatial, Stations and Weather data
-```sql 
+    
+-- add the weather
 create or replace view vhol_trips_stations_weather_vw as (
   select t.*, temp_avg_c, temp_avg_f,
          wind_dir, wind_speed_mph, wind_speed_kph
@@ -403,21 +291,13 @@ create or replace view vhol_trips_stations_weather_vw as (
        left outer join vhol_weather_vw w on date_trunc('day', starttime) = observation_date);
 
 
--- let's query the integrated data view
+-- let's review the integrated data view
 select * from vhol_trips_stations_vw limit 200;
 select * from vhol_trips_stations_weather_vw limit 200;
 
-```
 
-###
 
-### 
-
-<!-- ------------------------ -->
-
-<!-- ------------------------ -->
-## Secure Data Sharing
-```  
+/*-------------------------------------Secure Tennent Data----------------------------------*/
 create or replace table tenant (
     tenant_id number,
     tenant_description string,
@@ -504,10 +384,6 @@ insert into tenant_stations values
   (1, 298)
 ;
 
-```
-
-### Enabling Row Level Access Policy 
-```
 --select *
 select * from tenant_stations;
 
@@ -535,10 +411,7 @@ where
     tenant.tenant_id = $tenant_sv
 limit 100;
 
-```
 
-### Create Secure Objects to Share 
-```
 --secure view
 create or replace secure view  vhol_trips_secure as
 (select --tripduration, 
@@ -557,10 +430,6 @@ select current_account();
 
 select * from vhol_trips_secure limit 100;
 
-```
-
-### Create Reader Account 
-```
 --create a reader account for your tenant
 show managed accounts;
 DROP MANAGED ACCOUNT IMP_CLIENT;
@@ -587,10 +456,7 @@ alter session unset simulated_data_sharing_consumer;
 
 --are you sure?
 select count(*) from vhol_trips_secure;
-```
 
-### Grant Share Access to Reader 
-```
 --create share and share to reader account
 CREATE OR REPLACE SHARE VHOL_SHARE COMMENT='Creating my Share to Share with my Reader';
 GRANT USAGE ON DATABASE VHOL_DATABASE TO SHARE VHOL_SHARE;
@@ -604,14 +470,10 @@ SHOW SHARES LIKE 'VHOL_SHARE';
 show managed accounts;
 select  $1 as NAME ,$4 AS LOCATOR,$6 as URL FROM table (result_scan(last_query_id()));
 
-```
+/*-------------------------------------------------------------------------------------------------------------------
+-- </VHOL SQL>
+-------------------------------------------------------------------------------------------------------------------*/
 
-###
-
-<!-- ------------------------ -->
-
-<!-- ------------------------ -->
-## Data Consumer Access
 CREATE DATABASE TRIPSDB FROM SHARE 
 create or replace warehouse VHOL_READER WITH 
     WAREHOUSE_SIZE = 'XSMALL' 
@@ -630,65 +492,7 @@ USE SCHEMA VHOL_SCHEMA;
 
 SELECT * FROM VHOL_SCHEMA.VHOL_TRIPS_SECURE;
 
-<!-- ------------------------ -->
-
-### 
-<button>
-  [This is a download button](link.com)
-</button>
-
-### Tables
-<table>
-    <thead>
-        <tr>
-            <th colspan="2"> **The table header** </th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>The table body</td>
-            <td>with two columns</td>
-        </tr>
-    </tbody>
-</table>
-
-### Hyperlinking
-[Youtube - Halsey Playlists](https://www.youtube.com/user/iamhalsey/playlists)
-
-<!-- ------------------------ -->
-## Images, Videos, and Surveys, and iFrames
-Duration: 2
-
-Look at the [markdown source for this guide](https://raw.githubusercontent.com/Snowflake-Labs/sfguides/master/site/sfguides/sample.md) to see how to use markdown to generate these elements. 
-
-### Images
-![Snowflake Login](assets/new_snowflake_ui.png)
-
-### Videos
-Videos from youtube can be directly embedded:
-<video id="KmeiFXrZucE"></video>
-
-### Inline Surveys
-<form>
-  <name>How do you rate yourself as a user of Snowflake?</name>
-  <input type="radio" value="Beginner">
-  <input type="radio" value="Intermediate">
-  <input type="radio" value="Advanced">
-</form>
-
-### Embed an iframe
-![https://codepen.io/MarioD/embed/Prgeja](https://en.wikipedia.org/wiki/File:Example.jpg "Try Me Publisher")
-
-<!-- ------------------------ -->
-## Conclusion
-Duration: 1
-
-At the end of your Snowflake Guide, always have a clear call to action (CTA). This CTA could be a link to the docs pages, links to videos on youtube, a GitHub repo link, etc. 
-
-If you want to learn more about Snowflake Guide formatting, checkout the official documentation here: [Formatting Guide](https://github.com/googlecodelabs/tools/blob/master/FORMAT-GUIDE.md)
-
-### What we've covered
-- creating steps and setting duration
-- adding code snippets
-- embedding images, videos, and surveys
-- importing other markdown files
+---- Test User -- 
+--https://app.snowflake.com/us-west-2/vka53369
+--user: testvhol
+--pwd: Welcome@4533 
