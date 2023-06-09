917cbe873ebcd41675b4790342953ff3f045296c
jsun <james.sun@snowflake.com>
2023-06-09 10:30:02 -0700

    update to latest claat

--- site/sfguides/src/visualize_real-time_data_with_AMG_and_Streamlit/assets/deploy_cfn.sh
+++ site/sfguides/src/visualize_real-time_data_with_AMG_and_Streamlit/assets/deploy_cfn.sh
@@ -1,8 +1,8 @@
-#. /var/root/.aws/jzsun4
+. /var/root/.aws/jzsun4
 
 bucket='jsnow-vhol-assets'
 region="us-west-2"
 
 aws s3 sync ecs/ s3://$bucket/ecs/cfts/
 
-aws cloudformation create-stack --stack-name ecs-sf-`date "+%s"` --disable-rollback --template-body file://ecs/main.json --capabilities CAPABILITY_IAM --capabilities CAPABILITY_NAMED_IAM --region $region
+aws cloudformation create-stack --stack-name ecs-sf-`date "+%s"` --parameters ParameterKey=SnowflakeUser,ParameterValue=streaming_user --parameters ParameterKey=SnowflakePassword,ParameterValue=Test1234567 --parameters ParameterKey=SnowflakeAccount,ParameterValue=zab91778.us-east-1 --disable-rollback --template-body file://ecs/main.json --capabilities CAPABILITY_IAM --capabilities CAPABILITY_NAMED_IAM --region $region
