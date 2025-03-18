d56e4433fc4c90c01a4d3a205fd70c80f8d025d2
Chandra <chandra.nayak@snowflake.com>
2025-03-18 16:52:12 -0700

    Included Review Date

--- site/sfguides/src/visual_analytics_powered_by_snowflake_and_tableau/assets/query_iceberg.sql
+++ site/sfguides/src/visual_analytics_powered_by_snowflake_and_tableau/assets/query_iceberg.sql
@@ -14,51 +14,73 @@ CREATE OR REPLACE EXTERNAL VOLUME vol_tastybytes_truckreviews
             
         )ALLOW_WRITES=true; 
 
+-- Create Iceberg Tables to track metadata 
 CREATE OR REPLACE ICEBERG TABLE iceberg_truck_reviews
         (
         source_name VARCHAR,
-        quarter VARCHAR,
+        quarter varchar,
         order_id BIGINT,
         truck_id INT,
         language VARCHAR, 
         review VARCHAR,
         primary_city VARCHAR,
-        truck_brand VARCHAR
+        customer_id varchar,
+        year date,
+        month date,
+        truck_brand VARCHAR,
+        review_date date
         )
         CATALOG = 'SNOWFLAKE'
         EXTERNAL_VOLUME = 'vol_tastybytes_truckreviews'
         BASE_LOCATION = 'reviews-s3-volume'; 
 
--- Insert Iceberg Metadata from External Files 
+
+-- Insert  Metadata from External Files 
 INSERT INTO iceberg_truck_reviews
 (
-    order_id, quarter, truck_id, language, source_name, review, primary_city, truck_brand 
+        source_name,
+        quarter,
+        order_id,
+        truck_id,
+        language, 
+        review,
+        primary_city ,
+        customer_id ,
+        year ,
+        month ,
+        truck_brand ,
+        review_date 
 )
 SELECT 
+       SPLIT_PART(METADATA$FILENAME, '/', 4) as source_name,
+       CONCAT(SPLIT_PART(METADATA$FILENAME, '/', 2),'/' ,SPLIT_PART(METADATA$FILENAME, '/', 3)) as quarter,
        $1 as order_id,
-       SPLIT_PART(METADATA$FILENAME, '/', 2) as quarter,
        $2 as truck_id,
        $3 as language,
-       CONCAT(SPLIT_PART(METADATA$FILENAME, '/', 3),'/',SPLIT_PART(METADATA$FILENAME, '/', 4)) as source_name,
        $5 as review,
        $6 as primary_city, 
-       $10 as truck_brand 
+       $7 as customer_id,
+       $8 as year,
+       $9 as month,
+       $10 as truck_brand,
+      DATEADD(HOUR, (ROW_NUMBER() OVER(ORDER BY seq8())-3), to_date('01-11-2022','dd-mm-yyyy')) as review_date
 FROM @stg_truck_reviews 
 (FILE_FORMAT => 'FF_CSV',
-PATTERN => '.*reviews.*[.]csv');
+PATTERN => '.*reviews.*[.]csv') 
+;
 
 
 -- Create a view on the Iceberg Reviews, and run Cortex AI to extract Sentiment
 USE SCHEMA analytics;
 
 -- We have non-english reviews from global customers
-SELECT order_id, quarter, truck_id, language, source_name, primary_city, truck_brand , review where language !='en' limit 1;
+SELECT order_id, quarter, truck_id, language, source_name, primary_city, truck_brand , review, review_date where language !='en' limit 1;
 
 -- Snowflake Cortex makes it easy for us to translate and extract sentiment out of unstructured data
 CREATE OR REPLACE VIEW  frostbyte_tasty_bytes.analytics.product_unified_reviews as             
-    SELECT order_id, quarter, truck_id, language, source_name, primary_city, truck_brand , snowflake.cortex.sentiment(review) as final_review from frostbyte_tasty_bytes.raw_customer.iceberg_truck_reviews  where language='en'
+    SELECT order_id, quarter, truck_id, language, source_name, primary_city, truck_brand , snowflake.cortex.sentiment(review) , review_date as final_review from frostbyte_tasty_bytes.raw_customer.iceberg_truck_reviews  where language='en'
     UNION    
-    SELECT order_id, quarter, truck_id, language, source_name, primary_city, truck_brand , snowflake.cortex.sentiment(snowflake.cortex.translate(review,language,'en')) as final_review from frostbyte_tasty_bytes.raw_customer.iceberg_truck_reviews where language !='en';
+    SELECT order_id, quarter, truck_id, language, source_name, primary_city, truck_brand , snowflake.cortex.sentiment(snowflake.cortex.translate(review,language,'en')), review_date as final_review from frostbyte_tasty_bytes.raw_customer.iceberg_truck_reviews where language !='en';
 
 
 -- Sentiment Grouped By City and Brand 
